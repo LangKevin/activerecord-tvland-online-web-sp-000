4ebfd1b84091ba914ed6792cc7943f34eb88950f
kevin lang <langkevina@gmail.com>
2019-07-31 23:10:51 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,9 +1,3 @@
 class Show < ActiveRecord::Base
-  has_many :characters
-  has_many :actors, through: :characters
-  def actors_list
-    arr = []
-    arr << [self.actors.pluck(:first_name) + self.actors.pluck(:last_name)].flatten.join(" ")
-    # self.actors.pluck(:first_name, :last_name).first.flatten.join(" ")
-  end
+  Show belongs_to Network  
 end
