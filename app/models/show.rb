ed5c608d2eb0a55f5afcfc522a7dabe70b9b1f00
kevin lang <langkevina@gmail.com>
2019-07-31 23:25:08 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,7 +2,7 @@ class Show < ActiveRecord::Base
    has_many :characters
    belongs_to :network
    def actors_list
-    #  hash = {}
+     hash = {}
     #  self.characters.each |item|
       #  binding.pry
       #  hash = {name: item.actor.first_name + item.actor.last_name}
