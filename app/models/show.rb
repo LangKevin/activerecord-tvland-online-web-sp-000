19133527b106355c5155df066b315516d4e2033b
kevin lang <langkevina@gmail.com>
2019-07-31 23:31:17 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,7 +4,7 @@ class Show < ActiveRecord::Base
    def actors_list
      hash = {}
      self.characters.each do |item|
-       
+
        hash = {name: item.actor.first_name + " " item.actor.last_name}
        binding.pry
      end
